5de78dfa1c3e98e9d57d4934963aaed7582f49bf
kosei28 <contact@kosei28.com>
2024-06-23 00:40:36 +0900

    update

--- src/lib/api/app.ts
+++ src/lib/api/app.ts
@@ -1,8 +1,5 @@
 import { Handler, RouterBuilder } from 'effect-http';
 import { api, getHelloEndpoint } from './api';
-import { dev } from '$app/environment';
-import { NodeContext } from '@effect/platform-node';
-import { NodeSwaggerFiles } from 'effect-http-node';
 import { Effect } from 'effect';
 import { HttpApp } from '@effect/platform';
 
@@ -10,11 +7,10 @@ const getHelloHandler = Handler.make(getHelloEndpoint, ({ query }) =>
 	Effect.succeed({ message: `Hello, ${query.name}!` })
 );
 
-const app = RouterBuilder.make(api, { enableDocs: dev, docsPath: '/api/docs' }).pipe(
+const app = RouterBuilder.make(api, { enableDocs: false }).pipe(
 	RouterBuilder.handle(getHelloHandler),
-	RouterBuilder.build,
-	Effect.provide(NodeSwaggerFiles.SwaggerFilesLive),
-	Effect.provide(NodeContext.layer)
+	RouterBuilder.build
 );
 
-export const handler = HttpApp.toWebHandler(app);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const handler = HttpApp.toWebHandler(app as any);
